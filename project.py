e810384ab05054456a5c5efe62fc5e986e274719
Abass Hassan <Ahassan124@student.cscc.edu>
2026-06-17 16:36:01 -0400

    Added input for student name

--- project.py
+++ project.py
@@ -5,3 +5,6 @@ Purpose: A simple student registration system for courses.
 Resources: Course Chapters 1-7
 Date: June 2026
 """
+
+student_name = input("Enter your name: ")
+print("Welcome,", student_name)
